703119110df568f940a475a812bddad5cb6c3bba
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-02 04:42:38 -0300

    :sparkles: feat: adicona a classe Saint com métodos getters

--- src/br/com/fidesdiaria/domain/model/Saint.java
+++ src/br/com/fidesdiaria/domain/model/Saint.java
@@ -1,5 +1,25 @@
 package br.com.fidesdiaria.domain.model;
 
 public class Saint {
-
+	private String name;
+	private int day;
+	private int month;
+	
+	public Saint(String name, int day, int month) {
+		this.name = name;
+		this.day = day;
+		this.month = month;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getDay() {
+		return this.day;
+	}
+	
+	public int getMonth() {
+		return this.month;
+	}
 }
