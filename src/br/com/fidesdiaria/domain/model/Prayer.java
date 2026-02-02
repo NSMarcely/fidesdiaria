5dbc89f41360bfcd2967f5fdac6e0a7c7994a20e
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-02 05:12:15 -0300

    :sparkles: feat: implementa a classe Prayer com getters e setters

--- src/br/com/fidesdiaria/domain/model/Prayer.java
+++ src/br/com/fidesdiaria/domain/model/Prayer.java
@@ -1,5 +1,37 @@
 package br.com.fidesdiaria.domain.model;
+import java.time.LocalDate;
 
 public class Prayer {
+	private Long id;
+	private String type;
+	LocalDate fulfillment;
+	
+	public Prayer(String type) {
+		this.type = type;
+		this.id = null;
+		this.fulfillment = LocalDate.now();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getFulfillment() {
+		return fulfillment;
+	}
+
+	public void setFulfillment(LocalDate fulfillment) {
+		this.fulfillment = fulfillment;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	
 
 }
