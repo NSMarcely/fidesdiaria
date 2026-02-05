bc7c00281e2f031a1223fc1b0805d88c412ad7f7
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-04 22:53:23 -0300

    :recylce: refactor: alterar a interface PrayerRepository para PrayerRecordRepository

--- src/br/com/fidesdiaria/domain/repository/PrayerRecordRepository.java
+++ src/br/com/fidesdiaria/domain/repository/PrayerRecordRepository.java
@@ -2,7 +2,7 @@ package br.com.fidesdiaria.domain.repository;
 import java.util.List;
 import br.com.fidesdiaria.domain.model.Prayer;
 
-public interface PrayerRepository {
+public interface PrayerRecordRepository {
 	void createPrayer(Prayer prayer);
 
 	List<Prayer> findAll();
