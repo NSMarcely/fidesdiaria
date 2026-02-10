9f97eb95257a745a610808dbf851899a56e4559b
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-09 21:06:00 -0300

    :sparkles: feat(infrastructure): criar classe ReadingRepositoryTxt e implementar a interface ReadingRepository

---
+++ src/br/com/fidesdiaria/infrastructure/persistence/ReadingRepositoryTxt.java
@@ -0,0 +1,24 @@
+package br.com.fidesdiaria.infrastructure.persistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import br.com.fidesdiaria.domain.model.Reading;
+import br.com.fidesdiaria.domain.repository.ReadingRepository;
+
+public class ReadingRepositoryTxt implements ReadingRepository {
+	private final Map<Long, Reading> readings;
+	
+	public ReadingRepositoryTxt(Map<Long, Reading> readings) {
+        this.readings = readings;
+    }
+
+	@Override
+	public void createReading(Reading reading) {
+		this.readings.put(reading.getReadingId(), reading);
+	}
+	
+	@Override
+	public List<Reading> findAll(){
+		return new ArrayList<>(this.readings.values());
+	}
+}
--- src/br/com/fidesdiaria/infrastructure/persistence/UserRepositoryTxt.java
+++ src/br/com/fidesdiaria/infrastructure/persistence/UserRepositoryTxt.java
@@ -1,5 +1,29 @@
 package br.com.fidesdiaria.infrastructure.persistence;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import br.com.fidesdiaria.domain.model.User;
+import br.com.fidesdiaria.domain.repository.UserRepository;
 
-public class UserRepositoryTxt {
+public class UserRepositoryTxt implements UserRepository {
+	private final Map<Long,User> users = new HashMap<>();
+	
+	@Override
+	public void createUser(User user) {
+		this.users.put(user.getId(), user);
+	}
+	
+	@Override
+	
+	public Optional<User> findById(Long id){
+		return Optional.ofNullable(this.users.get(id));
+	}
+	
+	@Override
+	public List<User> findAll(){
+		return new ArrayList<>(this.users.values());
+	}
 
 }
