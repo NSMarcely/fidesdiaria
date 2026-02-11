cb89efa3d0fe712c6294633d7b73b085d87df8c2
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-10 21:58:34 -0300

    :sparkles: feat(application): validar senha do usuário com PasswordValidator

--- src/br/com/fidesdiaria/application/service/UserService.java
+++ src/br/com/fidesdiaria/application/service/UserService.java
@@ -1,6 +1,7 @@
 package br.com.fidesdiaria.application.service;
 import br.com.fidesdiaria.domain.model.User;
 import br.com.fidesdiaria.domain.repository.UserRepository;
+import br.com.fidesdiaria.domain.security.PasswordValidator;
 
 public class UserService {
 	private final UserRepository userRepository;
@@ -10,6 +11,9 @@ public class UserService {
 	}
 	
 	public void registerUser(String name, String password) {
+		if(!PasswordValidator.isValid(password)) {
+			throw new IllegalArgumentException("A senha não atende aos requisitos.");
+		}
 		User newUse = new User(name, password);
 		this.userRepository.createUser(newUse);
 	}
