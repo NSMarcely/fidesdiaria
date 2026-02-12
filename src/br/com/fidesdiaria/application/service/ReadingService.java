39ab4f1e00120b4f6bc1d19dfcdb9738c98aaf43
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-11 21:10:56 -0300

    :sparkles: feat(application): criar a classe ReadingService com o método registerReading

---
+++ src/br/com/fidesdiaria/application/service/ReadingService.java
@@ -0,0 +1,16 @@
+package br.com.fidesdiaria.application.service;
+import br.com.fidesdiaria.domain.model.Reading;
+import br.com.fidesdiaria.domain.repository.ReadingRepository;
+
+public class ReadingService {
+	private final ReadingRepository readingRepository;
+	
+	public ReadingService(ReadingRepository readingRepository) {
+		this.readingRepository = readingRepository;
+	}
+	
+	public void registerReading(Long userId, String verse, String reflection) {
+		Reading newReading = new Reading(userId, verse, reflection);
+		this.readingRepository.createReading(newReading);
+	}
+}
