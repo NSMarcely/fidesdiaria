7b7c359e928c5fffa553e59dfd074c369b5ccd4e
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-12 15:33:44 -0300

    :sparkles: feat(application): implementar updateProgress no método registerReading da classe ReadingService

--- src/br/com/fidesdiaria/application/service/ReadingService.java
+++ src/br/com/fidesdiaria/application/service/ReadingService.java
@@ -1,17 +1,22 @@
 package br.com.fidesdiaria.application.service;
 import br.com.fidesdiaria.domain.model.Reading;
 import br.com.fidesdiaria.domain.repository.ReadingRepository;
+import br.com.fidesdiaria.domain.repository.UserRepository;
 
 public class ReadingService {
 	private final ReadingRepository readingRepository;
+	private final UserRepository userRepository;
 	
-	public ReadingService(ReadingRepository readingRepository) {
+	public ReadingService(ReadingRepository readingRepository, UserRepository userRepository) {
 		this.readingRepository = readingRepository;
+		this.userRepository = userRepository;
 	}
 	
 	public void registerReading(Long userId, String verse, String reflection) {
+		short score = (short) (reflection.length() > 35 ? 10 : 5) ;
 		Reading newReading = new Reading(userId, verse, reflection);
 		this.readingRepository.createReading(newReading);
+		this.userRepository.updateProgress(userId, score);
 	}
 	
 	public void readingHistory(Long userId) {
