4064714b9142e6265a998bb6f1043c1de8188690
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-12 21:20:14 -0300

    :sparkles: feat(domain): adicinar na interface SaintRepository o método findAll

--- src/br/com/fidesdiaria/domain/repository/SaintRepository.java
+++ src/br/com/fidesdiaria/domain/repository/SaintRepository.java
@@ -1,5 +1,9 @@
 package br.com.fidesdiaria.domain.repository;
+import java.util.List;
+import br.com.fidesdiaria.domain.model.Saint;
 
 public interface SaintRepository {
 	void creatDaySaint(String name, int day, int month);
+	
+	List<Saint> findAll();
 }
