02e1ffd1fecae06f466e8d780370380ddd9e1528
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-17 22:39:29 -0300

    :sparkles: feat(application): criar a classe AdministratorService com o método promoteToAdmin

---
+++ src/br/com/fidesdiaria/application/service/AdministratorService.java
@@ -0,0 +1,23 @@
+package br.com.fidesdiaria.application.service;
+import br.com.fidesdiaria.domain.model.User;
+import br.com.fidesdiaria.domain.model.UserType;
+import br.com.fidesdiaria.domain.repository.UserRepository;
+
+public class AdministratorService {
+	private final UserRepository userRepository;
+	
+	public AdministratorService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public void promoteToAdmin(Long userId) {
+		User userFound =this.userRepository.findById(userId)
+				.orElseThrow(() -> new RuntimeException("Usuário não indetificado."));
+		
+		if(userFound.getType() == UserType.ADMINISTRATOR) {
+			throw new RuntimeException("O usuário já é administrador.");
+		}
+		userFound.setType(UserType.ADMINISTRATOR);
+	}
+}
+
