15f96e0d1ead3bb3e432d103731171df391d295b
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-19 22:41:11 -0300

    :sparkles: feat(domain): criar a classe NameFormatter com o método stático format

---
+++ src/br/com/fidesdiaria/domain/security/NameFormatter.java
@@ -0,0 +1,14 @@
+package br.com.fidesdiaria.domain.security;
+
+public class NameFormatter {
+	public static String format(String inputName) {
+		String name = inputName.trim();
+		String[] arrayName = name.split("\\s+");
+		StringBuilder newName = new StringBuilder();
+		for(String i : arrayName) {
+			String formatter = i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase();
+			newName.append(formatter).append(" ");
+		}
+		return arrayName.toString();
+	}
+}
