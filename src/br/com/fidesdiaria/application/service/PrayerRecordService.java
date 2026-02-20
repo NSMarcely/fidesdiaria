1f1a702cffe343f3ea9cd3a23cc0e38a1d87f9e3
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-19 23:00:07 -0300

    :sparkles: feat(application): implementar updateProgress no método registerPrayerRecord na classe PrayerRecordService

--- src/br/com/fidesdiaria/application/service/PrayerRecordService.java
+++ src/br/com/fidesdiaria/application/service/PrayerRecordService.java
@@ -2,17 +2,22 @@ package br.com.fidesdiaria.application.service;
 import br.com.fidesdiaria.domain.model.Prayer;
 import br.com.fidesdiaria.domain.model.PrayerRecord;
 import br.com.fidesdiaria.domain.repository.PrayerRecordRepository;
+import br.com.fidesdiaria.domain.repository.UserRepository;
 
 public class PrayerRecordService {
 	private final PrayerRecordRepository prayerRecordRepository;
+	private final UserRepository userRepository;
 	
-	public PrayerRecordService(PrayerRecordRepository prayerRecordRepository) {
+	public PrayerRecordService(PrayerRecordRepository prayerRecordRepository, UserRepository userRepository) {
 		this.prayerRecordRepository = prayerRecordRepository;
+		this.userRepository = userRepository;
 	}
 	
-	public void resgisterPrayerRecord(Long userid, String type) {
+	public void registerPrayerRecord(Long userid, String type) {
+		short score = 5;
 		Prayer prayerType = Prayer.fromString(type);
 		PrayerRecord prayerRecord = new PrayerRecord(userid, prayerType);
 		this.prayerRecordRepository.createPrayer(prayerRecord);
+		this.userRepository.updateProgress(userid, score);
 	}
 }
