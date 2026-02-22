bf72e2e965edcebcbeba4f33cc964b656ce7e288
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-21 22:29:23 -0300

    :sparkles: feat(domain): adicionar condição no método format da classe NameFormatter

--- src/br/com/fidesdiaria/domain/security/NameFormatter.java
+++ src/br/com/fidesdiaria/domain/security/NameFormatter.java
@@ -2,6 +2,9 @@ package br.com.fidesdiaria.domain.security;
 
 public class NameFormatter {
 	public static String format(String inputName) {
+		if(inputName.isBlank() || inputName == null) {
+			throw new RuntimeException("Escampo não pode estar vazio");
+		}
 		String name = inputName.trim();
 		String[] arrayName = name.split("\\s+");
 		StringBuilder newName = new StringBuilder();
@@ -9,6 +12,6 @@ public class NameFormatter {
 			String formatter = i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase();
 			newName.append(formatter).append(" ");
 		}
-		return arrayName.toString();
+		return arrayName.toString().trim();
 	}
 }
