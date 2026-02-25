171bc0912a0f7f57796c105bb775ceae8915e929
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-02-24 23:18:40 -0300

    :sparkles:  feat(domain): adicionar a classe PrayerRecord o atributo prayerId e o métodos toString

--- src/br/com/fidesdiaria/domain/model/PrayerRecord.java
+++ src/br/com/fidesdiaria/domain/model/PrayerRecord.java
@@ -4,14 +4,21 @@ import java.time.LocalDate;
 public class PrayerRecord {
 	private Long userId;
 	private Prayer type;
+	private Long PrayerId;
 	private LocalDate fulfillment;
 	
 	public PrayerRecord(Long userId, Prayer type) {
 		this.userId = userId;
 		this.type = type;
+		this.PrayerId = null;
 		this.fulfillment = LocalDate.now();
 	}
 	
+	public String toString() {
+		return String.format("\nID: %d"
+				+ "\nTipo: %s"
+				+ "\nData: %tD", this.PrayerId, this.type.getPrayerName(), this.fulfillment);
+	}
 	public Long getUserId() {
 		return this.userId;
 	}
@@ -19,7 +26,15 @@ public class PrayerRecord {
 	public Prayer getType() {
 		return this.type;
 	}
-
+	
+	public Long getPrayerId() {
+		return this.PrayerId;
+	}
+	
+	public void setPrayerId(Long id) {
+		this.PrayerId = id;
+	}
+	
 	public LocalDate getFulfillment() {
 		return this.fulfillment;
 	}
