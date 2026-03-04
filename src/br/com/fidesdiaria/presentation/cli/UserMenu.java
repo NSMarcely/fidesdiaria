ee006ba1ca1009834974e5d0bac61385ca0d6192
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-03-03 22:09:50 -0300

    :recycle: refactor: modularizar UserMenu separando exibição, processamento e controle de fluxo

--- src/br/com/fidesdiaria/presentation/cli/UserMenu.java
+++ src/br/com/fidesdiaria/presentation/cli/UserMenu.java
@@ -2,59 +2,84 @@ package br.com.fidesdiaria.presentation.cli;
 import java.util.Scanner;
 import br.com.fidesdiaria.application.service.PrayerRecordService;
 import br.com.fidesdiaria.application.service.ReadingService;
+import br.com.fidesdiaria.application.service.SaintService;
 import br.com.fidesdiaria.application.service.UserService;
 
 public class UserMenu {
+	
 	private final UserService userService;
 	private final PrayerRecordService prayerRecordService;
 	private final ReadingService readingService;
-	public UserMenu(UserService userService, PrayerRecordService prayerRecordService, ReadingService readingService) {
+	private final SaintService saintService;
+	
+	public UserMenu(UserService userService, PrayerRecordService prayerRecordService, ReadingService readingService, SaintService saintService) {
 		this.userService = userService;
 		this.prayerRecordService = prayerRecordService;
 		this.readingService = readingService;
+		this.saintService = saintService;
 	}
 	
-	public void view(Long userId) {
+	public void showCommonUserOptions(Long userId) {
+		System.out.println("::Menu::\n");
+		this.saintService.findDaySaint();
+		this.userService.viewUserProfile(userId);
+		System.out.println("1 - Registrar oração."
+				+ "\n2- Registrar leitura bíblica."
+				+ "\n3- Ver histórico de oração."
+				+ "\n4- Ver histórico de leitura bíblica.");
+	}
+	
+	public void processOptions(int option, Scanner input, Long userId) {
+		switch(option){
+		case 1:
+			input.nextLine();
+			this.prayerRecordService.viewTypesPrayer();
+			System.out.println("Tipo: ");
+			String type = input.nextLine();
+			this.prayerRecordService.registerPrayerRecord(userId, type);
+			break;
+		
+		case 2: 
+			System.out.println("Versículo lido: ");
+			String verse = input.nextLine();
+			System.out.println("Reflexão: ");
+			String reflection = input.nextLine();
+			this.readingService.registerReading(userId, verse, reflection);
+			break;
+		case 3:
+			this.prayerRecordService.prayerHistory(userId);
+			break;
+		case 4:
+			this.readingService.readingHistory(userId);
+			break;
+		default:
+			System.out.println("Opção inválida.");
+			break;
+		}
+	}
+	
+	public void displayCommonMenu(Long userId) {
+		
 		Scanner input = new Scanner(System.in);
 		boolean finish = false;
 		while(!finish) {
-			this.userService.viewUserProfile(userId);
-			System.out.println("1 - Registrar oração."
-					+ "\n2- Registrar leitura bíblica."
-					+ "\n3- Ver histórico de oração."
-					+ "\n4- Ver histórico de leitura bíblica."
-					+ "\n5- Sair.");
+			this.showCommonUserOptions(userId);
+			System.out.println("5- Sair.");
+			System.out.println("Opção: ");
 			int option = input.nextInt();
-			input.nextLine();
-			switch(option){
-			case 1:
-				this.prayerRecordService.viewTypesPrayer();
-				System.out.println("Tipo:");
-				String type = input.nextLine();
-				this.prayerRecordService.registerPrayerRecord(userId, type);
-				break;
-			
-			case 2: 
-				System.out.println("Versículo lido:");
-				String verse = input.nextLine();
-				System.out.println("Reflexão: ");
-				String reflection = input.nextLine();
-				this.readingService.registerReading(userId, verse, reflection);
-				break;
-			case 3:
-				this.prayerRecordService.prayerHistory(userId);
-				break;
-			case 4:
-				this.readingService.readingHistory(userId);
-				break;
-			case 5:
+			if(option == 5) {
 				System.out.println("Saindo do sistema...");
 				return;
-			default:
-				System.out.println("Opção inválida.");
-				break;
+			}
+			else {
+				processOptions(option, input, userId);
 			}
 		}
 		input.close();
 	}
+	
+	public SaintService getSaintService() {
+		return this.saintService;
+	}
+	
 }
